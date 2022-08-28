061cb11044f1ce307f709f454fc8c694023c9a3f
Timothy Wu <twu23@buffalo.edu>
2022-08-27 21:35:49 -0400

    reverted env variable

--- src/main/java/dev/wu/utils/ConnectionUtil.java
+++ src/main/java/dev/wu/utils/ConnectionUtil.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class ConnectionUtil {
     public static Connection createConnection() {
         try {
-            return DriverManager.getConnection(System.getenv("project1db"));
+            return DriverManager.getConnection(System.getenv("Project1DB"));
         } catch (SQLException e) {
             e.printStackTrace();
             return null;
